f735b9f1b395cdd5aea70135a240003cceb819e2
doxliydriw <123301390+doxliydriw@users.noreply.github.com>
2023-06-06 17:15:21 +0200

    update

--- five.js
+++ five.js
@@ -1,3 +1,6 @@
 let first_nmbr = prompt("Please enter 5 digit number to split", "number#1");
-let symbols = first_nmbr.split ("");
-alert('Split result is ' + symbols)
+res = []
+for (i in first_nmbr) {
+    res +=  first_nmbr[i] + ' '
+}
+alert('Split result is ' + res)
